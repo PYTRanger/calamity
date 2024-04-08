2c93fed309c0e53ef31e880f83ac6231c713acb2
PYTRanger <1406utkarsh@gmail.com>
2024-04-08 11:00:53 +0530

    improved css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="p-6 bg-white rounded shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Calamity</h1>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
-          <a href="/api/hello">Get</a>
-        </button>
+    <main className="flex items-center justify-center h-screen bg-black">
+      <div className="p-6 bg-gray-900 rounded shadow-md">
+        <h1 className="text-6xl font-calvier mb-4 text-white">Calamity</h1>
+        <div className="flex justify-center">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+            <a href="/api/hello">Get</a>
+          </button>
+        </div>
       </div>  
     </main>
   );
